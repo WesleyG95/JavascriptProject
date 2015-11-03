fd1c0539c6e07bce139a67d844dd39fa36c75648
WesleyG95 <WesleyG95@hotmail.com>
2015-11-03 13:48:05 -0500

    Fixed JSON input

--- js/script.js
+++ js/script.js
@@ -4,13 +4,16 @@ $('#search').keyup(function(){
 	$.getJSON('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=b38114a0-1167-4959-8137-98e9e3103f8c', function(data){
 		var output = '<ul class="searchresult">';
 		$.each(data, function(key, val){
-			if((val.name.search(myExp) != -1) || (val.bio.search(myExp) != -1)) {
+			console.log(val);
+			/*
+			if(val.name.search(myExp) != -1) {
 				output +='<li>';
 				output +='<h2>' + val.name + '</h2>';
 				output +='<img src="images/' + val.shortname + '_tn.jpg" alt="'+ val.name +'" />';
 				output +='<p>' + val.bio + '</p>';
 				output +='</li>';
 			}
+			*/
 		});
 		output += '</ul>';
 		$('#update').html(output);
