4649c472f7b3ab2bd9aa2fe19a9ed17e2ded2cd1
WesleyG95 <WesleyG95@hotmail.com>
2015-11-09 09:50:31 -0500

    Changed Search Box

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 $('#search').keyup(function(){
 	var searchField = $('#search').val();
 	var myExp = new RegExp(searchField, 'i');
-	$.getJSON('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=b38114a0-1167-4959-8137-98e9e3103f8c', function(results){
+	$.getJSON('http://ddragon.leagueoflegends.com/cdn/5.22.1/data/en_US/champion.json', function(results){
 		var output = '<ul class="searchresult">';
 		
 		$.each(results.data, function(key, val) {
 			if(val.name.search(myExp) != -1)
 			{
+				val.name = val.name.replace(/\s+/g, '');
 				output +='<li>';
-				output +='<h2>' + name + '</h2>';
-				output +='<img src="http://ddragon.leagueoflegends.com/cdn/5.22.1/img/champion/' + val.name + '.png"></img>';
+				output +='<h2>' + val.name + '</h2>';
+				output +='<img src="http://ddragon.leagueoflegends.com/cdn/5.22.1/img/champion/' + val.image.full + '"></img>';
 				output +='<p>' + val.title + '</p>';
+				output +='<p>' + val.blurb + '</p>';
 				output +='</li>';
 			}
 		});
