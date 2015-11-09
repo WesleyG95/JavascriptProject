fb61b1b7f2b6e3d6fd86bae449b2a520fd77ed24
WesleyG95 <WesleyG95@hotmail.com>
2015-11-09 08:06:00 -0500

    Fixed search

--- js/script.js
+++ js/script.js
@@ -1,21 +1,43 @@
 $('#search').keyup(function(){
 	var searchField = $('#search').val();
 	var myExp = new RegExp(searchField, 'i');
+	$.getJSON('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=b38114a0-1167-4959-8137-98e9e3103f8c', function(results){
+		var output = '<ul class="searchresult">';
+		
+		$.each(results.data, function(key, val) {
+			if(val.name.search(myExp) != -1)
+			{
+				output +='<li>';
+				output +='<h2>' + name + '</h2>';
+				output +='<img src="http://ddragon.leagueoflegends.com/cdn/5.22.1/img/champion/' + val.name + '.png"></img>';
+				output +='<p>' + val.title + '</p>';
+				output +='</li>';
+			}
+		});
+		
+		output += '</ul>';
+		$('#update').html(output);
+		
+	});
+});
+
+/*
+$('#search').keyup(function(){
+	var searchField = $('#search').val();
+	var myExp = new RegExp(searchField, 'i');
 	$.getJSON('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=b38114a0-1167-4959-8137-98e9e3103f8c', function(data){
 		var output = '<ul class="searchresult">';
 		$.each(data, function(key, val){
-			console.log(val);
-			/*
-			if(val.name.search(myExp) != -1) {
+			if((val.name.search(myExp) != -1) || (val.bio.search(myExp) != -1)) {
 				output +='<li>';
 				output +='<h2>' + val.name + '</h2>';
 				output +='<img src="images/' + val.shortname + '_tn.jpg" alt="'+ val.name +'" />';
 				output +='<p>' + val.bio + '</p>';
 				output +='</li>';
 			}
-			*/
 		});
 		output += '</ul>';
 		$('#update').html(output);
 	});
 });
+*/
